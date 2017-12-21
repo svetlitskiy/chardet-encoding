33e30ecda6957fbcf05666ee162f1cc0f160ca61
Vladislav.Sharikov <vladislav.sharikov@billing.ru>
2017-12-21 17:01:22 +0300

    test: more tests, more files and add snap

--- test/my-test.spec.js
+++ test/my-test.spec.js
@@ -1,21 +1,23 @@
 const { executeCommand } = require('./helpers/common');
-const path = require('path');
 
 jest.setTimeout(60000);
 
 const TEST_PROJECT_DIR = 'test/test-project';
 
-test('test', async () => {
+test('test utf8 target', async () => {
   await executeCommand('npm run clear', TEST_PROJECT_DIR);
   await executeCommand('npm i', TEST_PROJECT_DIR);
-  const result = await executeCommand('npx grunt encoding', TEST_PROJECT_DIR);
+  const result = await executeCommand('npx grunt encoding:utf8', TEST_PROJECT_DIR);
 
   expect(result).toMatchSnapshot();
+});
 
-/*  await executeCommand('npm i', TEST_PROJECT_DIR);
-  const testResults = await executeCommand('npm test', TEST_PROJECT_DIR);
+test('test win1251 target', async () => {
+  await executeCommand('npm run clear', TEST_PROJECT_DIR);
+  await executeCommand('npm i', TEST_PROJECT_DIR);
+  const result = await executeCommand('npx grunt encoding:win1251', TEST_PROJECT_DIR);
 
-  expect(testResults).toMatchSnapshot();*/
+  expect(result).toMatchSnapshot();
 });
 
 function kek () {
